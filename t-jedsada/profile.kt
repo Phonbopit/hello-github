fe84f801ccea8ea72a64d66d305c34d807517e52
Jedsada Tiwongvorakul <jt@20scoops.net>
2017-10-13 22:29:42 +0700

    add new file.

--- t-jedsada/profile.kt
+++ t-jedsada/profile.kt
@@ -1,6 +1,3 @@
-var profile = {
-  "name": "Jedsada",
-  "position": "Android Developer <3 Kotlin",
-  "age": "2x",
-  "website: "https://medium.com/@pondthaitay"
+fun main(args: Array<String>) {
+    print("Hello World")
 }
